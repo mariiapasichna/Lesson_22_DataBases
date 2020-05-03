b0844351cdf21980132a093e6b1209b9f9136ad6
Mariia Pasichna <pasichnayam@gmail.com>
2020-05-03 11:34:46 +0300

    correct getConfigurations()

--- src/main/java/com/mariiapasichna/dao/Configurations.java
+++ src/main/java/com/mariiapasichna/dao/Configurations.java
@@ -9,8 +9,10 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Configurations {
+    private static SessionFactory sessionFactory;
 
     public static SessionFactory getConfigurations() {
+        if (sessionFactory == null || sessionFactory.isClosed()) {
         Configuration configuration = new Configuration();
         configuration.configure("hibernate.cfg.xml");
         Properties properties = getPropertiesFile();
@@ -18,7 +20,9 @@ public class Configurations {
         String password = properties.getProperty("password");
         configuration.getProperties().setProperty("hibernate.connection.password", password);
         configuration.getProperties().setProperty("hibernate.connection.username", username);
-        return configuration.buildSessionFactory();
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        return sessionFactory;
     }
 
     private static Properties getPropertiesFile() {
